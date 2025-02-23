848bfee400bf79001ccd22fceb483e0720188595
Enric <enricgalerarotger@gmail.com>
2025-02-23 11:54:43 +0100

    Delete php directory

--- php/confirm.php
+++
@@ -1,7 +0,0 @@
-<?php
-// echo "Hello World!<br/>";
-// echo "I contain a lot of PHP code!<br/>";
-// echo "Uncomment the code in confirm.php to see the output.";
-$input = $_POST['input'];
-echo "You entered: $input";
-?>
