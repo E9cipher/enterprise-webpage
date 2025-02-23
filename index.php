bdb130ac245357b4a4abf0dc7b35a8d12034c10e
Enric <enricgalerarotger@gmail.com>
2025-02-23 14:14:05 +0100

    Refactor styles and implement database insertion in confirm.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Company - Homepage</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="stylesheet" href="styles-webpage.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <script src="https://kit.fontawesome.com/822c48f9f0.js" crossorigin="anonymous"></script>
--- php/confirm.php
+++ php/confirm.php
@@ -1,5 +1,22 @@
 <?php
-echo "Hello World!";
+// echo "Hello World!";
+// Collect data from the form
 $email = $_POST['email'];
-echo $email;
+// echo $email;
+// Create a DB connection
+$conn = new mysqli('localhost', 'scripts', 'Scripts123!', 'php');
+// Check for connection error
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}
+
+// Insert data into the DB
+$sql = "INSERT INTO users (email) VALUES ('$email')";
+if($conn->query($sql) === TRUE){
+    echo "New record created successfully";
+}else{
+    echo "Error: ".$sql."<br>".$conn->error;
+}
+// Close the connection
+$conn->close();
 ?>
